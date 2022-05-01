e34b8632ea16d3ca0c069b27a435d51e6057f388
Stalkerfish <silva.allison2002@gmail.com>
2022-05-01 19:05:39 -0300

    create ROS workspace added 'roscpp' and 'rospy'

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/atlas/environment/ros-publisher-subscriber/ros_ws/src
+# Build directory: /home/atlas/environment/ros-publisher-subscriber/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
