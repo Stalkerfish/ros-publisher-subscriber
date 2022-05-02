e3ce02d28d1228e8a8064ad3ccaaef8a6c792b88
Stalkerfish <silva.allison2002@gmail.com>
2022-05-02 00:26:28 -0300

    making a publisher node sends 'I'm Publishing at 2Hz' to a topic called my_topic

--- ros_ws/src/pubpub/src/my_pub.cpp
+++ ros_ws/src/pubpub/src/my_pub.cpp
@@ -1,15 +1,21 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main (int argc, char **argv){
 	ros::init(argc, argv, "my_pub");
 	ros::NodeHandle nh;
-	
+
+	ros::Publisher pub = nh.advertise<std_msgs::String>("/my_topic", 10);
+
+
 	ROS_INFO("Node has been started");
 
-	ros::Rate rate(10);
+	ros::Rate rate(2);
 
 	while (ros::ok()){
-		ROS_INFO("Hi, There!");
+		std_msgs::String msg;
+		msg.data = "I'm Publishing!";
+		pub.publish(msg);
 		rate.sleep();
 	}
 }
